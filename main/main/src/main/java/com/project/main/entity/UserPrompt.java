52fa85826ab4ca4eae433a32c9b39ad2044e2301
Angela <angela.lai237@gmail.com>
2024-09-05 21:30:40 +1000

    refactor: add Column annotations to UserPrompt and related setter/getter methods

--- main/main/src/main/java/com/project/main/entity/UserPrompt.java
+++ main/main/src/main/java/com/project/main/entity/UserPrompt.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 
 import java.time.LocalDateTime;
 
@@ -11,13 +12,23 @@ import java.time.LocalDateTime;
 public class UserPrompt {
 
     @Id
+    @Column(name="prompt_id")
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer promptId;
 
+    @Column(name="search_prompt")
     private String searchPrompt;
+
+    @Column(name="date_time")
     private LocalDateTime dateTime;
+
+    @Column(name="selected_filter")
     private String selectedFilter;
+
+    @Column(name="view_preference")
     private String viewPreference;
+
+    @Column(name="graph_view_type")
     private String graphViewType;
 
     private UserPrompt() {}
@@ -38,16 +49,32 @@ public class UserPrompt {
         return this.searchPrompt;
     }
 
+    public void setSearchPrompt(String searchPrompt) {
+        this.searchPrompt = searchPrompt;
+    }
+
     public String getSelectedFilter() {
         return this.selectedFilter;
+    }
+
+    public void setSelectedFilter(String selectedFilter) {
+        this.selectedFilter = selectedFilter;
     }
 
     public String getViewPreference() {
         return this.viewPreference;
     }
 
+    public void setViewPreference(String viewPreference) {
+        this.viewPreference = viewPreference;
+    }
+
     public String getGraphViewType() {
         return this.graphViewType;
+    }
+
+    public void setGraphViewType(String graphViewType) {
+        this.graphViewType = graphViewType;
     }
 
     public LocalDateTime getDateTime() {
