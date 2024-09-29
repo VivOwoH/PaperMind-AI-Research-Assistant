669646cdaf9ca13bf545434712fcb599da33b33c
Angela <alai7251@uni.sydney.edu.au>
2024-09-29 17:03:46 +1000

    feat: remove filter options for opinion based graphs in frontend

--- main/main/frontend/src/components/SearchComponent.jsx
+++ main/main/frontend/src/components/SearchComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {TextField, Radio, RadioGroup, FormControlLabel, FormControl,FormLabel,InputAdornment,Typography} from '@mui/material';
+import {TextField, Radio, RadioGroup, FormControlLabel, FormControl,InputAdornment,Typography} from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import '../styles/components/SearchComponent.css';
@@ -39,21 +39,22 @@ export default function SearchComponent(){
 
     return (
         <div>
-        <div>
-        <FormControl component="fieldset" className="options-fieldset">
-            {/* Radio buttons for filtering by all / supporting / opposing ideas */}
-            <FormLabel component="legend"></FormLabel>
-                {/* set default pick as 'all' */}
-                <RadioGroup value={category} onChange={(e) =>
-                     {setCategory(e.target.value);
-                        updateSearchData('selectedFilter',e.target.value);
-                     }}>
-                    <FormControlLabel className="radio-label" value="ALL" control={<Radio />} label="All" style={{ paddingBottom: '2px' }}/>
-                    <FormControlLabel className="radio-label" value="SUPPORTING" control={<Radio />} label="Supporting" style={{ paddingBottom: '2px' }} />
-                    <FormControlLabel className="radio-label"  value="OPPOSING" control={<Radio />} label="Opposing" style={{ paddingBottom: '2px' }} />
+        {searchData.graphViewType === 'CITATION' && (
+            <FormControl component="fieldset" className="options-fieldset">
+                {/* Radio buttons for filtering by all / supporting / opposing ideas */}
+                <RadioGroup
+                    value={category}
+                    onChange={(e) => {
+                        setCategory(e.target.value);
+                        updateSearchData('selectedFilter', e.target.value);
+                    }}
+                >
+                    <FormControlLabel className="radio-label" value="ALL" control={<Radio />} label="All" />
+                    <FormControlLabel className="radio-label" value="SUPPORTING" control={<Radio />} label="Supporting" />
+                    <FormControlLabel className="radio-label" value="OPPOSING" control={<Radio />} label="Opposing" />
                 </RadioGroup>
-        </FormControl>
-        </div>
+            </FormControl>
+        )}
 
         <Typography variant="h6">Search Topic</Typography>
         <div className="search-container">
@@ -68,16 +69,16 @@ export default function SearchComponent(){
             }}
             className="search-input"
             InputProps={{
-            endAdornment: (
-            <InputAdornment position="end">
-            <IconButton
-                onClick={handleSearch}
-                edge="end"
-            >
-            <SearchIcon />
-            </IconButton>
-            </InputAdornment>
-            )
+                endAdornment: (
+                    <InputAdornment position="end">
+                        <IconButton
+                            onClick={handleSearch}
+                            edge="end"
+                        >
+                            <SearchIcon />
+                        </IconButton>
+                    </InputAdornment>
+                )
             }}
             />
         </div>
