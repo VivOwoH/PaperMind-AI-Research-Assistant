75233488a2085ee9b0b14392c3052a9ae5639df3
Vivian Ha <weha7612@uni.sydney.edu.au>
2024-09-29 18:37:26 +1000

    feat: semantic api

--- main/main/src/main/java/com/project/main/controller/APIController.java
+++ main/main/src/main/java/com/project/main/controller/APIController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,20 +15,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.project.main.entity.UserPrompt;
 import com.project.main.service.GeminiService;
+import com.project.main.service.SemanticService;
 import com.project.main.service.UserPromptService;
 
 @RestController
 @RequestMapping("/api/data")
 @CrossOrigin(origins = "http://localhost:3000")
-public class GeminiController {
+public class APIController {
 
 	@Autowired
 	GeminiService geminiService;
 
 	private final UserPromptService userPromptService;
+	private final SemanticService semanticService;
 
-	public GeminiController(UserPromptService userPromptService) {
+	public APIController(UserPromptService userPromptService, SemanticService semanticService) {
 		this.userPromptService = userPromptService;
+		this.semanticService = semanticService;
 	}
 
 	@PostMapping
@@ -44,14 +46,30 @@ public class GeminiController {
 		 * must structure response like this: "keywords:keyword1, keyword2 etc.; sentiment:positive/negative;"
 		 */
 		String formattedPrompt = String.format(
-				"extract key words and determine positive or negative sentiment for statement '%s'; must structure response like this: 'keywords:keyword1, keyword2 etc.; sentiment:positive/negative;'",
+				"extract key words and determine positive or negative sentiment for statement '%s', only keep neural terms (non-sentimental terms) as keywords; must structure response like this: 'keywords:keyword1, keyword2 etc.; sentiment:positive/negative;'",
 				userPrompt.getSearchPrompt());
 
 		userPrompt.setSearchPrompt(formattedPrompt); // reset user prompt to tuned prompt
-		String responseBody = geminiService.callApi(userPrompt, "AIzaSyDLmB0f1-lmo2-WH9Dif0fC32t0_Z9Hpuo"); // TODO:
-																											// encrypt
-																											// key
+		String responseBody = geminiService.callApi(userPrompt, "AIzaSyDLmB0f1-lmo2-WH9Dif0fC32t0_Z9Hpuo"); // TODO: encrypt key
+		
 		// response formatting
+		ObjectNode responseJSON = responseFormatting(responseBody).getBody();
+
+		System.out.println(responseJSON);
+
+		// semantic: use the list of extracted keywords to get a JSON list of papers
+		String semanticQuery = responseJSON.get("keywords").asText()
+						.replace(",", "")
+						.replaceAll(";$", "").trim();
+		String fetchedPapers = semanticService.paperRelevanceSearch(semanticQuery);
+		
+		// gemini: tuned_prompt for top 10 papers
+
+		// output: JSON list of 10 papers, or input required for the graph back to react via request
+		return ResponseEntity.ok(fetchedPapers);
+	}
+
+	public ResponseEntity<ObjectNode> responseFormatting(String responseBody) {
 		ObjectMapper objectMapper = new ObjectMapper();
 		try {
 			JsonNode jsonNode = objectMapper.readTree(responseBody);
@@ -68,19 +86,11 @@ public class GeminiController {
 			responseJson.put("keywords", keywords);
 			responseJson.put("sentiment", sentiment);
 
-			System.out.println(responseJson);
-			return ResponseEntity.ok(responseJson.toString());
-
-		} catch (JsonProcessingException e) {
+			return ResponseEntity.ok(responseJson);
+		} 
+		catch (JsonProcessingException e) {
 			e.printStackTrace();
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing JSON");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
 		}
-
-		// semantic: use the list of extracted keywords to get a JSON list of papers
-
-		// gemini: tuned_prompt for top 10 papers
-
-		// output: JSON list of 10 papers, or input required for the graph back to react
-		// via request
 	}
 }
